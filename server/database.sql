226d3b5f737e61cbdf632a8b1c265f9ac6eacb4e
amilcar <amilcar.pio@gmail.com>
2023-07-11 21:10:39 -0300

    fix: ajuste nas config. do db

--- server/database.sql
+++ server/database.sql
@@ -3,5 +3,8 @@ CREATE DATABASE contatinhos
 CREATE TABLE contatos(
     contato_id SERIAL PRIMARY KEY,
     nome VARCHAR(50),
-    numero VARCHAR(11)
+    numero VARCHAR(15),
+    email VARCHAR(50),
+    cargo VARCHAR(25),
+    sexo VARCHAR(25)
 )
--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
-const pool = new Pool ({
-    user:'amilcarpio',
-    password:'267800',
-    host: 'localhost',
-    database: 'contatinhos',
-    port: 5432
+const pool = new Pool({
+  user: 'INSERIR USER AQUI',
+  password: 'INSERIR SENHA AQUI',
+  host: 'localhost',
+  database: 'contatinhos',
+  port: 5432
 })
 
-module.exports = pool;
+module.exports = pool
